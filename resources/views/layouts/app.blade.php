123825ecf34fc10599a40795b759dd8196067859
makhosandile <makho@fastacademy>
2018-04-03 09:09:58 +0200

    added the app name in the nevbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,10 +22,10 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel ">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <b>{{ config('', 'Students Tasks plan') }}</b>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
